9faeab81792779b85afe92f57bc5b27b8d2da82a
Robert Bubnik <rbubnik@gmail.com>
2024-08-22 21:15:53 -0400

    passing all tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+    name:"Harold",
+    streetAddress:"36-36 202st"
+}
+
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
